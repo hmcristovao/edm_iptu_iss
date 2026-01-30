633248f8b8e14e97b6ed7e273b690c315eac7c32
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2026-01-30 09:14:30 -0300

    Add header and footer parameters to Parameters class
    The Parameters class constructor now accepts 'header' and 'footer' integer arguments, which are stored as instance attributes. This change allows for more flexible configuration of header and footer values.

--- src/Domain/Parameters.py
+++ src/Domain/Parameters.py
@@ -1,6 +1,8 @@
 class Parameters():
-    def __init__(self, pasta: str, saida: str, sufixo: list[str], variaveis: list = None):
+    def __init__(self, pasta: str,footer:int, header:int, saida: str, sufixo: list[str], variaveis: list = None):
         self.pasta = pasta
         self.saida = saida
+        self.header =  header
+        self.footer = footer
         self.sufixo = sufixo
         self.variaveis = variaveis or []
